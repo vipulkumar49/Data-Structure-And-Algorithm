802b98b063f37fb6cbda820e47fed165f4897b90
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-06-22 22:27:42 +0530

    Time: 37 ms (7.28%), Space: 13.4 MB (10.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size(); 
-        vector<int> rows, columns;
+        int m = matrix.size(), n = matrix[0].size();
+        unordered_set<int> rows, columns;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    rows.push_back(i);
-                    columns.push_back(j);
+                    rows.insert(i);
+                    columns.insert(j);
                 }
             }
         }
-        for(int i = 0; i < rows.size(); i++) {
+        for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                matrix[rows[i]][j] = 0;
-            }
-        }
-        for(int j = 0; j < columns.size(); j++) {
-            for(int i = 0; i < m; i++) {
-                matrix[i][columns[j]] = 0;
+                if(rows.find(i) != rows.end() || columns.find(j) != columns.end()) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
