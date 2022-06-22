e28ff8e987348837695491487c6e5ca4ad2ef353
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-06-22 22:36:09 +0530

    Time: 19 ms (65.63%), Space: 13 MB (95.76%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        unordered_set<int> rows, columns;
+        bool isColZero = false;
         for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+            if(matrix[i][0] == 0) {
+                isColZero = true;
+            } 
+            for(int j = 1; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    rows.insert(i);
-                    columns.insert(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(rows.find(i) != rows.end() || columns.find(j) != columns.end()) {
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(isColZero) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
