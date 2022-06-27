dc311a85d8c439c7f6bbb688939c5ab0355bd3b6
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-06-27 13:00:12 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -8,13 +8,12 @@ using namespace std;
 class Solution{
     public:
     long long countWays(int n, int k){
-        long long total = k;
-        // same - prev two fence have same color
-        // diff - prev two fence have diff color
-        long long same = 0;
-        long long diff = k;
         long long mod = 1e9 + 7;
-        for(int i = 2; i <= n; i++) {
+        if(n == 1) return k % mod;
+        long long same = k % mod;
+        long long diff = ((k % mod) * ((k - 1) % mod)) % mod;
+        long long total = (same + diff) % mod;
+        for(int i = 3; i <= n; i++) {
             same = diff % mod;
             diff = ((total) % mod * (k - 1) % mod) % mod;
             total = ((same) % mod + (diff) % mod) % mod;
