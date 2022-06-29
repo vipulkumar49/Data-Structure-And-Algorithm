ba1c63936584adde0f66177b69b60a14af38b356
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-06-29 10:42:57 +0530

    Time: 25 ms (17.39%), Space: 12 MB (61.24%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -4,7 +4,13 @@ public:
         stack<int> s;
         int n = tokens.size();
         for(int i = 0; i < n; i++) {
-            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+            if(isdigit(tokens[i][0])) {
+                s.push(stoi(tokens[i]));
+            }
+            else if(tokens[i][0] == '-' && tokens[i].length() >= 2) {
+                s.push(stoi(tokens[i]));
+            }
+            else {
                 int a = s.top();
                 s.pop();
                 int b = s.top();
@@ -15,9 +21,6 @@ public:
                 else if(tokens[i] == "*") c = a * b;
                 else c = b / a;
                 s.push(c);
-            }
-            else {
-                s.push(stoi(tokens[i]));
             }
         }
         return s.top();
