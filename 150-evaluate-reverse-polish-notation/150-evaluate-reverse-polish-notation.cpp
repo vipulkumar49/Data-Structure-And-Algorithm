261a113e2ff66e0cfc5c11f4bd8dede1e799717a
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-06-29 10:45:14 +0530

    Time: 25 ms (17.39%), Space: 11.9 MB (61.24%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -3,12 +3,12 @@ public:
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
         int n = tokens.size();
-        for(int i = 0; i < n; i++) {
-            if(isdigit(tokens[i][0])) {
-                s.push(stoi(tokens[i]));
+        for(auto const &t : tokens) {
+            if(isdigit(t[0])) {
+                s.push(stoi(t));
             }
-            else if(tokens[i][0] == '-' && tokens[i].length() >= 2) {
-                s.push(stoi(tokens[i]));
+            else if(t[0] == '-' && t.length() >= 2) {
+                s.push(stoi(t));
             }
             else {
                 int a = s.top();
@@ -16,9 +16,9 @@ public:
                 int b = s.top();
                 s.pop();
                 int c = 0;
-                if(tokens[i] == "+") c = a + b;
-                else if(tokens[i] == "-") c = b - a;
-                else if(tokens[i] == "*") c = a * b;
+                if(t == "+") c = a + b;
+                else if(t == "-") c = b - a;
+                else if(t == "*") c = a * b;
                 else c = b / a;
                 s.push(c);
             }
