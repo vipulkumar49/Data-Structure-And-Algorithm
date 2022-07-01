055f859cedbb0eb74aff23df0861a295b532da3e
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-01 21:40:12 +0530

    Time: 36 ms (28.09%), Space: 27 MB (18.40%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInterleave(string s1, string s2, string s3, int i, int j, int k, vector<vector<int>> &dp) {
+        int n = s1.size(), m = s2.size();
+        if(i == n && j == m && k == s3.size()) {
+            return true;
+        }
+        if(k == s3.size()) {
+            return false;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        bool res = false;
+        if(s1[i] == s3[k]) res = checkInterleave(s1, s2, s3, i + 1, j, k + 1, dp);
+        if(res == true) return dp[i][j] = true;
+        if(s2[j] == s3[k]) res = checkInterleave(s1, s2, s3, i, j + 1, k + 1, dp); 
+        if(res == true) return dp[i][j] = true;
+        else  return dp[i][j] = false; 
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return checkInterleave(s1, s2, s3, 0, 0, 0, dp);
+    }
+};
