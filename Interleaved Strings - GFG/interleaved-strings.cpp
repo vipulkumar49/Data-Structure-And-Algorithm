d9fcc5400e13a9c53c1b847b5203b75873293e88
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-01 21:22:10 +0530

    Added solution - LeetHub

--- Interleaved Strings - GFG/interleaved-strings.cpp
+++ Interleaved Strings - GFG/interleaved-strings.cpp
@@ -10,24 +10,24 @@ using namespace std;
 class Solution{
   public:
     /*You are required to complete this method */
-    bool getInterleave(string A, string B, string C, int i, int j, int k, vector<vector<vector<int>>> &dp) {
+    bool getInterleave(string A, string B, string C, int i, int j, int k, vector<vector<int>> &dp) {
         int n = A.length(), m = B.length();
         if(k == n + m) {
             return true;
         }
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+        if(dp[i][j] != -1) return dp[i][j];
         
         bool res = false;
         if(A[i] == C[k]) res = getInterleave(A, B, C, i + 1, j, k + 1, dp);
-        if(res == true) return dp[i][j][k] = true;
+        if(res == true) return dp[i][j] = true;
         
         if(B[j] == C[k]) res = getInterleave(A, B, C, i, j + 1, k + 1, dp);
-        if(res == true) return dp[i][j][k] = true;
-        else return dp[i][j][k] = false;
+        if(res == true) return dp[i][j] = true;
+        else return dp[i][j] = false;
     }
     bool isInterleave(string A, string B, string C) {
         int n = A.length(), m = B.length();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(m + 1, vector<int>(n + m + 1, - 1)));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
         if(getInterleave(A, B, C, 0, 0, 0, dp)) return true;
         else return false;
     }
