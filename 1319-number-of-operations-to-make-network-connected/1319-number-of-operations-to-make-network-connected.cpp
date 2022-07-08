ad00fbba895c31ec26d77b776f7598ca6d2573c9
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-08 18:04:07 +0530

    Time: 145 ms (88.93%), Space: 37.8 MB (90.01%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void union_set(int a, int b, vector<int> &parent, vector<int> &rank) {
+        a = find_set(a, parent);
+        b = find_set(b, parent);
+        if(rank[a] > rank[b]) {
+            parent[b] = a;
+        }
+        else if(rank[b] > rank[a]) {
+            parent[a] = b;
+        }
+        else {
+            parent[b] = a;
+            rank[a] += 1; 
+        }
+    }
+    int find_set(int v, vector<int> &parent) {
+        if(parent[v] == v) {
+            return v;
+        }
+        return parent[v] = find_set(parent[v], parent);
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int m = connections.size();
+        if(m < n - 1) return -1;
+        vector<int> parent(n);
+        vector<int> rank(n, 0);
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        for(int i = 0; i < m; i++) {
+            union_set(connections[i][0], connections[i][1], parent, rank);
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(parent[i] == i) ans++;
+        }
+        return ans - 1;
+    }
+};
