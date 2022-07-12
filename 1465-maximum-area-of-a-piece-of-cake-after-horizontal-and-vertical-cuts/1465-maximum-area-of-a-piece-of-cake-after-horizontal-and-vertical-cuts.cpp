9bfdd0eded904e067e5599de20d74c53ee179758
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-12 12:21:36 +0530

    Time: 81 ms (91.20%), Space: 32.4 MB (39.15%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -3,20 +3,18 @@ public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
         sort(verticalCuts.begin(), verticalCuts.end());
         sort(horizontalCuts.begin(), horizontalCuts.end());
-        int max_w = 0, max_h = 0, curr_w = 0, curr_h = 0;
+        int max_w = 0, max_h = 0;
         long long mod = 1e9 + 7;
         int n = horizontalCuts.size();
         for(int i = 0; i < n; i++) {
-            if(i == 0) curr_h = horizontalCuts[i];
-            else curr_h = horizontalCuts[i] - horizontalCuts[i - 1];
-            max_h = max(max_h, curr_h);
+            if(i == 0) max_h = max(max_h, horizontalCuts[i]);
+            else max_h = max(max_h, horizontalCuts[i] - horizontalCuts[i - 1]);
         }
         max_h = max(max_h, h - horizontalCuts[n - 1]);
         int m = verticalCuts.size();
         for(int i = 0; i < m; i++) {
-            if(i == 0) curr_w = verticalCuts[i];
-            else curr_w = verticalCuts[i] - verticalCuts[i - 1];
-            max_w = max(max_w, curr_w);
+            if(i == 0) max_w = max(max_w, verticalCuts[i]);
+            else max_w = max(max_w, verticalCuts[i] - verticalCuts[i - 1]);
         }
         max_w = max(max_w, w - verticalCuts[m - 1]);
         return ((max_h % mod) * (max_w % mod)) % mod;
