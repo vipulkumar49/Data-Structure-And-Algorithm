1a1e54af344fe496e803af704b1257d71951c1aa
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-12 09:53:12 +0530

    Time: 5 ms (61.72%), Space: 6.6 MB (63.00%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    bool checkInterleave(string &s1, string &s2, string &s3, int i, int j, int k, vector<vector<int>> &dp) {
-        int n = s1.size(), m = s2.size();
-        if(i == n && j == m) return true;
-        if(dp[i][j] != -1) return dp[i][j];
-        bool res = false;
-        if(s1[i] == s3[k]) res = checkInterleave(s1, s2, s3, i + 1, j, k + 1, dp);
-        if(res == true) return dp[i][j] = true;
-        if(s2[j] == s3[k]) res = checkInterleave(s1, s2, s3, i, j + 1, k + 1, dp); 
-        if(res == true) return dp[i][j] = true;
-        else  return dp[i][j] = false; 
-    }
     bool isInterleave(string s1, string s2, string s3) {
         if(s3.size() != s1.size() + s2.size()) return false;
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return checkInterleave(s1, s2, s3, 0, 0, 0, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                if(i == 0 && j == 0) dp[i][j] = true;
+                else if(i == 0) dp[i][j] = dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+                else if(j == 0) dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+                else 
+                    dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||  (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+            }
+        }
+        return dp[n][m];
     }
 };
