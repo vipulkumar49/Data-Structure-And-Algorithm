bb464d0338f55af76bd883bf306daef7fcbe7e49
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-23 16:30:17 +0530

    Time: 18 ms (89.96%), Space: 26.4 MB (28.28%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,23 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* buildBinaryTree(vector<int> &preorder, vector<int> &inorder, int ps, int pe, int is, int ie) {
+    TreeNode* buildBinaryTree(vector<int> &preorder, vector<int> &inorder, int ps, int pe, int is, int ie, unordered_map<int, int> &inorder_index) {
         if(is > ie) return NULL;
         
         int root_value = preorder[ps];
         TreeNode* root = new TreeNode(root_value);
         // get the pos of root
-        int pos = -1;
-        for(int i = is; i <= ie; i++) {
-            if(inorder[i] == root_value) pos = i;
-        }
+        int pos = inorder_index[root_value];
         // recursive call on left and right subtree
-        root->left = buildBinaryTree(preorder, inorder, ps + 1, ps + pos - is, is, pos - 1); 
-        root->right = buildBinaryTree(preorder, inorder, ps + pos - is + 1, pe, pos + 1, ie);
+        root->left = buildBinaryTree(preorder, inorder, ps + 1, ps + pos - is, is, pos - 1, inorder_index); 
+        root->right = buildBinaryTree(preorder, inorder, ps + pos - is + 1, pe, pos + 1, ie, inorder_index);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size(); // size of array 
-        return buildBinaryTree(preorder, inorder, 0, n - 1, 0, n - 1);
+        unordered_map<int, int> inorder_index;
+        for(int i = 0; i < n; i++) {
+            inorder_index[inorder[i]] = i;
+        }
+        return buildBinaryTree(preorder, inorder, 0, n - 1, 0, n - 1, inorder_index);
     }
 };
