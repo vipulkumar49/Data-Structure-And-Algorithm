f29fd5988689e1338133c69e81c18a6ba320d217
vipulkumar49 <95136363+vipulkumar49@users.noreply.github.com>
2022-07-31 14:11:58 +0530

    Time: 327 ms (100.00%), Space: 101.5 MB (100.00%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    void dfs(int node, vector<int> &edges, int d, vector<int> &len, vector<int> &visited, int &res) {   
-        if(visited[node] == false) {
-            visited[node] = true;
-            len[node] = d;
-            if(edges[node] != -1) {
-                dfs(edges[node], edges, d + 1, len, visited, res);
-            }
-            len[node] = -1;
+    void dfs(vector<int> &edges, int node, vector<int> &len, int d, vector<int> &visited, int &res) {
+        if(visited[node] == true) {
+            if(len[node] != -1)
+                res = max(res, d - len[node]);
         }
         else {
-            if(len[node] != -1) {
-                res = max(res, d - len[node]);
-            }
+            visited[node] = true; 
+            len[node] = d;
+            if(edges[node] != -1)
+                dfs(edges, edges[node], len, d + 1, visited, res);
+            len[node] = -1;
         }
-        
     }
     int longestCycle(vector<int>& edges) {
         int res = -1;
@@ -23,10 +20,9 @@ public:
         vector<int> len(n, -1);
         for(int i = 0; i < n; i++) {
             if(visited[i] == false) {
-                dfs(i, edges, 0, len, visited, res);
+                dfs(edges, i, len, 0, visited, res);
             }
         }
-        if(res == 0) return -1;
-        else return res;
+        return res;
     }
 };
